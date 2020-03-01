4f9488953cf0ccaa65487aa448386b23f5ce1e19
gaoyu <406261232>
2020-03-01 13:00:14 +0800

    初始化项目-安装ElementUI

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import ElementUI from 'element-ui' // 引入模块
+import 'element-ui/lib/theme-chalk/index.css' // 引入样式
 Vue.config.productionTip = false
-
+Vue.use(ElementUI) // 全局注册element-ui框架 一旦注册所有组件都可以用
 new Vue({
-  router,
-  render: h => h(App)
+    router,
+    render: h => h(App)
 }).$mount('#app')
